017ebbc3c57e84bdd293c84bd693b8723acd4744
bryant-con-t <bry.y.valen@gmail.com>
2025-11-12 01:21:29 -0500

    Agregar archivos de proyecto.

---
+++ POOII-cibertec-demo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace POOII_cibertec_demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
